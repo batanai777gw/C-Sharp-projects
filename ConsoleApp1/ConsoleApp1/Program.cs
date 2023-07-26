50b8ed835af0babf862bcda74f4a08ad6125e440
batanai777gw <batanai178@gmail.com>
2023-07-26 03:52:13 +0200

    first-commit

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{ //signifiies that our program is starting
+    //write code and the computer will execute that
+    class Program //a class is container where we can put code
+    {
+
+        static void Main(string[] args) //main method,
+        {
+
+            Console.WriteLine("Hello World!");
+
+            Console.ReadLine();
+        }
+    }
+}
